1dd4e3e222a7bb4464b19f298db2eb3bcc1cfdf1
chocoshop <15ec112j@rikkyo.ac.jp>
2021-02-11 21:15:47 +0900

    jsxを変換できるように設定をした

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,19 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.join(__dirname, 'build/'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }
+        ]
     }
 }
