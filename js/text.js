b985712927bddf188e29b2be04bd1599caf75c73
chocoshop <15ec112j@rikkyo.ac.jp>
2021-02-20 18:57:23 +0900

    明かりがチラつく文字のコンポーネント作った

---
+++ js/text.js
@@ -0,0 +1,21 @@
+import { Keyframes } from 'react-spring/renderprops'
+import React from 'react';
+
+export default function Text(props) {
+    const Container = Keyframes.Spring(async next => {
+        while(true) {
+            await next({
+                from: {opacity: 0.3},
+                opacity: 0.4,
+                reset: true
+            })
+            await next({
+                to: { opacity: 0.9 },
+                reset: true
+            })
+        }
+    })
+    return(
+        <Container config={{ velocity: 100, delay: Math.random() * 4500 }} >{styles => <div style={styles}>{props.children}</div>}</Container>
+    )
+}
